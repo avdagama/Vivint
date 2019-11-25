0acc162c78ea23ceb755f36e197b64d7af0aa291
Alex DaGama <47162944+avdagama@users.noreply.github.com>
2019-11-25 00:04:57 -0500

    Connected consultation footer to contact form using a GET request

---
+++ consultHandoff.js
@@ -0,0 +1,19 @@
+function getQueryParams(qs) {
+    qs = qs.split('+').join(' ');
+
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
+
+var query = getQueryParams(document.location.search);
+if (query.email==undefined) {}
+else  {
+  document.getElementById("emailInput").value = query.email;
+}
